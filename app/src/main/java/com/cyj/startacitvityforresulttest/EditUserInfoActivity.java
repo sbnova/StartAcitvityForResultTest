308a5a32ee5b108cadc48e8e29ef443d86fe971e
test <test1>
2018-12-29 12:22:58 +0900

    메인화면, 사용자 정보입력화면 분리

---
+++ app/src/main/java/com/cyj/startacitvityforresulttest/EditUserInfoActivity.java
@@ -0,0 +1,32 @@
+package com.cyj.startacitvityforresulttest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditUserInfoActivity extends BaseActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_user_info);
+
+        bindViews();
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+
+    }
+}
--- app/src/main/java/com/cyj/startacitvityforresulttest/MainActivity.java
+++ app/src/main/java/com/cyj/startacitvityforresulttest/MainActivity.java
@@ -1,9 +1,15 @@
 package com.cyj.startacitvityforresulttest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends BaseActivity {
+
+    private android.widget.TextView nameTxt;
+    private android.widget.Button nameInputBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +23,13 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
+        nameInputBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, EditUserInfoActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
@@ -27,6 +40,8 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void bindViews() {
+        this.nameInputBtn = (Button) findViewById(R.id.nameInputBtn);
+        this.nameTxt = (TextView) findViewById(R.id.nameTxt);
 
     }
 }
