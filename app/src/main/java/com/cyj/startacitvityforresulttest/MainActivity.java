6f1dc9a31d92c0d592f97a2201254db40656cbb6
test <test1>
2018-12-29 12:16:43 +0900

    Initial commit

---
+++ app/src/main/java/com/cyj/startacitvityforresulttest/MainActivity.java
@@ -0,0 +1,32 @@
+package com.cyj.startacitvityforresulttest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bindViews();
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+
+    }
+}
